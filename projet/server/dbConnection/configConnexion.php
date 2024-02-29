10b6cd1681679a9fb15c3fd34bcd8037c18b715a
Touristiquement <romain.benedetti@studentfr.ch>
2024-02-29 14:23:22 +0100

    Starting PHP

--- projet/server/dbConnection/configConnexion.php
+++ projet/server/dbConnection/configConnexion.php
@@ -2,7 +2,7 @@
 	//Informations de connexion à la base de données
 	define('DB_TYPE', 'mysql');
     define('DB_HOST', 'host.docker.internal');
-    define('DB_NAME', 'bd_TourisTunes');
-    define('DB_USER', 'root');
-    define('DB_PASS', 'emf123');
+    define('DB_NAME', 'db_TourisTunes');
+    define('DB_USER', 'benedettir');
+    define('DB_PASS', 'Je sais faire... 1553');
 ?>
